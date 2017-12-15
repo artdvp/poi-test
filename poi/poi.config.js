4f000efa15a6405d123b91f5c146407bf391b9bd
bonbonpa <darksphin@gmail.com>
2017-12-15 09:49:16 +0700

    add poi

---
+++ poi/poi.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    html: {
+        title: "My Artdvp App",
+        description: "My Description Artdvp",
+        template: "./index.ejs",
+        headers: "Artdvp Point",
+        contrib: ["Artdvp", "Jefhy"]
+    }
+}
